a9b0681247d009d94d870500cca4cf9739e07064
Drew Russell <Drew.Russell@rubrik.com>
2019-04-02 18:35:44 -0500

    update integration test

--- tests/create-cloud-cluster/main.tf
+++ tests/create-cloud-cluster/main.tf
@@ -1,26 +1,28 @@
-data "local_file" "random-number" {
+# Terraform Configuration used for full integration test
+
+resource "random_id" "r" {
+  byte_length = 1
+}
+
+resource "local_file" "random-number" {
+  content  = "${random_id.r.dec}"
   filename = "../random-number"
 }
 
-data "aws_instance" "cloud-cluster" {
-  filter {
-    name   = "tag:Name"
-    values = ["terraform-module-cloud-out-testing-${data.local_file.random-number.content}-1"]
-  }
+variable "aws_vpc_security_group_ids" {
+  type = "list"
 }
 
-provider "rubrik" {
-  node_ip  = "${data.aws_instance.cloud-cluster.private_ip}"
-  username = "admin"
-  password = "RubrikGoForward"
-}
+variable "aws_subnet_id" {}
 
-module "rubrik-s3-cloudout" {
-  source               = "../.."
-  bucket_name          = "tf-module-cloudout-test-${data.local_file.random-number.content}"
-  archive_name         = "S3:TerraformCloudOutModule"
-  bucket_force_destory = true
-  iam_user_name        = "tf-module-cloudout-test-user-${data.local_file.random-number.content}"
-  iam_policy_name      = "tf-module-cloudout-test-policy-${data.local_file.random-number.content}"
-  kms_key_alias        = "tf-module-cloudout-test-policy-kms-${data.local_file.random-number.content}"
+module "rubrik_aws_cloud_cluster" {
+  source = "rubrikinc/rubrik-cloud-cluster/aws"
+
+  aws_disable_api_termination = false
+  aws_vpc_security_group_ids  = "${var.aws_vpc_security_group_ids}"
+  aws_subnet_id               = "${var.aws_subnet_id}"
+  cluster_name                = "terraform-module-cloud-out-testing-${random_id.r.dec}"
+  admin_email                 = "build@rubrik.com"
+  dns_search_domain           = ["rubrikbuild.com"]
+  dns_name_servers            = ["8.8.8.8"]
 }
