093f50bcdaa71f79c880f0598c7d73d97cfa6c34
Damani <damani@rubrik.com>
2021-05-20 02:23:01 -0700

    Added outputs to config CloudOut due to mod. move

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,31 @@
-output "aws_iam_user_name" {
-  description = "The name of the AWS IAM User created."
-  value       = "${aws_iam_user.rubrik.name}"
+data "template_file" "iam_secret_key" {
+  template = aws_iam_access_key.rubrik-user.secret
 }
 
-output "rubrik_archive_name" {
-  description = "The name of the archival location created on the Rubrik cluster."
-  value       = "${rubrik_aws_s3_cloudout.archive-target.archive_name}"
+output "aws_iam_user_info" {
+  description = "The information about the AWS IAM User created."
+  value       = {
+    "name"               = aws_iam_user.rubrik.name
+    "aws_iam_access_key" = aws_iam_access_key.rubrik-user.id
+    "aws_iam_secret_key" = data.template_file.iam_secret_key.rendered
+  }
+}
+
+output "aws_iam_policy" {
+  description = "The name ofthe AWS Policy created for the IAM User."
+  value       = aws_iam_policy.cloud-out-permissions.name
+}
+
+output "aws_bucket" {
+  description = "The AWS S3 bucket that was created."
+  value       = aws_s3_bucket.archive_target.bucket
+}
+
+output "aws_kms_key" {
+  description = "The KMS Key ID of the KMS key that was created."
+  value       = aws_kms_key.rubrik-cloudout.key_id
+}
+output "aws_region" {
+  description = "The AWS region where the resoruces were created."
+  value       = var.region
 }
